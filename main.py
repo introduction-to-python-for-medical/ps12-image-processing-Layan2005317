a36570c8bfb8a07b7d0b1c82d09382d7f567842a
Layan2005317 <layan.khamisa@gmail.com>
2025-02-03 14:35:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from image_utils import load_image, edge_detection
+from PIL import Image
+import numpy as np
+from skimage.filters import median
+from skimage.morphology import ball
 
+path='lena.jpeg'
+image=load_image(path)
+clean_image = median(image, ball(3))
+edge_mag=edge_detection(clean_image)
+edge_binary=edge_mag<100
+edge_image = Image.fromarray((edge_binary*255).astype(np.uint8))
+edge_image.save('my_edges.jpeg')
